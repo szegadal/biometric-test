00bc9fed7ef25b0901a000678964bda4ec36e111
kinitroConsultoria <sebastian.zegada@gmail.com>
2024-09-24 11:19:41 -0400

    url de dev en metabet service

--- src/services/metabet.js
+++ src/services/metabet.js
@@ -1,6 +1,6 @@
 // const urlBase = 'https://system.cpelsac.com/api/v1'
-const urlBase = 'http://localhost:3002/api/v1'
-// const urlBase = 'https://401n8gz0-3001.brs.devtunnels.ms/api/v1'
+// const urlBase = 'http://localhost:3002/api/v1'
+const urlBase = 'https://401n8gz0-3002.brs.devtunnels.ms/api/v1'
 // const urlBase = 'https://st5v6tkh-3001.brs.devtunnels.ms/api/v1'
 
 export async function startBiometricSession(requestData) {
